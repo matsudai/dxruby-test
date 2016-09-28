cafcb07face755a13227491c3e7be3242987c3eb
s1619 <s1619>
2016-09-28 14:06:01 +0900

    Mitigate process hit-check
    あたり判定の処理軽減
    self::check()をEnemy::update()で呼ばずにObserver::update()で呼ぶようにした
    処理負荷は軽減されたがSpriteクラスを継承したクラスにhit()をオーバーライドしてObserverにあたり判定をとりたいオブジェクトについてSprite::check()を呼ばなきゃならん

--- test.rb
+++ test.rb
@@ -18,15 +18,20 @@ class Enemy < Sprite
     @costume = {live:Image.new(20, 20, C_GREEN), dead:Image.new(20, 20, C_RED)}
     super x, y, @costume[:live]
   end
+=begin
   def update game_objects
     hit_objects = self.check game_objects.select {|obj| obj.instance_of? Player}
     self.image = @costume[:dead] unless hit_objects.empty?
+  end
+=end
+  def hit obj
+    self.image = @costume[:dead] if obj.instance_of? Player
   end
 end
 
 player = Player.new Input.mouse_x, Input.mouse_y
 
-enemies = 5.times.map {|enemy|
+enemies = 200.times.map {|enemy|
     Enemy.new rand * 600, rand * 440
 }
 
@@ -44,6 +49,8 @@ class Observer
     else
       raise "Observer::<<() : arg is NOT Array-class or Sprite-subclass !!"
     end
+    @players = @game_objects.select {|obj| obj.instance_of? Player}
+    @enemies = @game_objects.select {|obj| obj.instance_of? Enemy}
     self
   end
   def update
@@ -51,6 +58,7 @@ class Observer
       next obj.update if obj.method(:update).parameters.length.zero?
       obj.update @game_objects
     }
+    @players.each {|player| Sprite.check player, @enemies}
   end
   def draw
     @game_objects.each {|obj| obj.draw}
@@ -60,7 +68,6 @@ end
 Observer.instance << player << enemies
 
 Window.width, Window.height = 640, 480
-
 
 Window.loop do
   break if Input.key_push? K_ESCAPE
